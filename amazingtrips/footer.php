1a4855a6389ab8a139bd5a8ff30861092bed01c8
Imane <113670994+imanemeradi@users.noreply.github.com>
2022-10-24 15:28:29 +0200

    Update footer.php

--- amazingtrips/footer.php
+++ amazingtrips/footer.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<link rel="stylesheet" href="accueill.css">
+<link rel="stylesheet" href="stylesheet.css">
 <meta charset="utf-8">
 <footer id="footer">
 	<p>© 2022 AmazingTrips, Inc. Tous droits réservés</p>

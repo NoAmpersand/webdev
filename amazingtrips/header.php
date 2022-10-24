c1fc5125e7b8cc87cebef46ab88c6ad66d5148dc
Imane <113670994+imanemeradi@users.noreply.github.com>
2022-10-24 15:43:24 +0200

    Update header.php

--- amazingtrips/header.php
+++ amazingtrips/header.php
@@ -7,7 +7,7 @@
 	<table id="tablo1">
 	<tr><td><img src="logovoyage.jpeg" alt="Logo site" width="100" height="100"></td>
 		
-		<nav><td align="right" id="tablo"><span class="button"> <a href="accueill.php" class="menu">Accueil </a> </span> &emsp;&ensp;  <span class="button"> <a href="#Nos voyages" class="menu">Nos voyages </a> </span>  &emsp;&ensp; <span class="button"><a href="connexion.php" class="menu">Connexion </a></span></td></nav>
+		<nav><td align="right" id="tablo"><span class="button"> <a href="accueill.php" class="menu">Accueil </a> </span> &emsp;&ensp;  <span class="button"> <a href="#Nos voyages" class="menu">Nos voyages </a> </span>  &emsp;&ensp; <span class="button"><a href="connexion.php" class="menu">Connexion </a></span>&emsp;&ensp; <span class="button"><a href="Inscription.php" class="menu">Inscription </a></span></td></nav>
 	</tr>
 
 </table>

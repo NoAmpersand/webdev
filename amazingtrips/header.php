4b34ac3709a1effdd50ca073932acb0235b7a7ad
Imane <113670994+imanemeradi@users.noreply.github.com>
2022-10-24 12:05:23 +0200

    Add files via upload
    header

---
+++ amazingtrips/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+
+<header>
+	<meta charset="utf-8">
+	<link rel="icon" type="image/jpeg" sizes="16x16" href="logovoyage.jpeg">
+	<table id="tablo1">
+	<tr><td><img src="logovoyage.jpeg" alt="Logo site" width="100" height="100"></td>
+		
+		<nav><td align="right" id="tablo"><span class="button"> <a href="accueil.html" class="menu">Accueil </a> </span> &emsp;&ensp;  <span class="button"> <a href="#Nos voyages" class="menu">Nos voyages </a> </span>  &emsp;&ensp; <span class="button"><a href="connection.php" class="menu">Connexion </a></span></td></nav>
+	</tr>
+
+</table>
+</header>
